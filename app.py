647c5b59c3f64d206a7dacb3f15a6fc5d1c26b1c
Kai <107215518+kaikendoh@users.noreply.github.com>
2022-07-20 02:59:12 -0500

    added perks.txt file to cover shortcuts and common mistakes, added error handling

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd
 
 from twitchio.ext import commands
 from config import TWITCH_OAUTH_TOKEN
+
+# Import perks csv as dataframe
+perks_df = pd.read_csv('perks.txt')
 
 # Twitch channels for the bot to listen to
 twitch_channels = ['kaikendoh', 'bongokaibot']
@@ -11,15 +15,20 @@ twitch_channels = ['kaikendoh', 'bongokaibot']
 # Function to change the case of perk to capitalize the first letter
 # except for certain words
 def case_except(s):
-    exceptions = ['a', 'an', 'of', 'the', 'is', 'for', 'de', 'from']
+    exceptions = ['a', 'an', 'of', 'the', 'is', 'for', 'de', 'from', 'with']
     word_list = re.split(' ', s)
     final = [word_list[0].capitalize()]
     for word in word_list[1:]:
         final.append(word if word in exceptions else word.capitalize())
     return " ".join(final)
 
+# Function to scrape the perk from the dbd fandom wiki
 def perk_scrape(perk):
-    perkurl = case_except(perk).replace(" ", "_")
+    if perks_df['perk_name'].eq(perk).any():
+        perkurl = perks_df.loc[perks_df['perk_name'] == perk, 'perk_url'].values[0]
+    else:
+        perkurl = case_except(perk).strip().replace(" ", "_")
+
     url = "https://deadbydaylight.fandom.com/wiki/{a}".format(a=perkurl)
 
     response = requests.get(url)
@@ -27,9 +36,10 @@ def perk_scrape(perk):
 
     soup = BeautifulSoup(webpage, 'html.parser')
 
-    fullperk = soup.find('div', class_='formattedPerkDesc').get_text(separator=' ', strip=True).replace('\n', '')
+    perk_name = soup.find('h1').get_text(strip=True)
+    perk_desc = soup.find('div', class_='formattedPerkDesc').get_text(separator=' ', strip=True).replace('\n', '')
 
-    return fullperk
+    return perk_name, perk_desc
 
 
 class Bot(commands.Bot):
@@ -59,11 +69,13 @@ class Bot(commands.Bot):
         # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
 
+    # hello command
     @commands.command()
     async def hello(self, ctx: commands.Context):
         await ctx.send(f'Hello {ctx.author.name}!')
         await ctx.send('2nd message')
     
+    # test command
     @commands.command()
     async def test(self, ctx: commands.Context, *, message):
         if message == 'test':
@@ -71,27 +83,40 @@ class Bot(commands.Bot):
         else:
             await ctx.send(f"You didn't type test! You typed {message}")
 
+    # perk command
     @commands.command()
     async def perk(self, ctx:commands.Context, *, perk):
-        perk_desc = perk_scrape(perk)
-        if len(perk_desc) <= 500:
-            await ctx.send(perk_desc)
-        else:
-            sets = round(len(perk_desc)/500 + 0.5)
+        # take perk from chat message and run through perk_scrape function
+        try:
+            perk_name, perk_desc = perk_scrape(perk)
+            perk_full = perk_name + ' - ' + perk_desc
 
-            for i in range(sets):
-                if i == 0:
-                    s_index = perk_desc[:494].rfind(' ')
-                    await ctx.send('(' + str(i + 1) + '/' + str(sets) + ') ' + perk_desc[:s_index])
-                    i += 1
-                    perk_desc = perk_desc[s_index + 1:]
-                elif i == sets - 1:
-                    await ctx.send('(' + str(i + 1) + '/' + str(sets) + ') ' + perk_desc)
-                else:
-                    s_index = perk_desc[:494].rfind(' ')
-                    await ctx.send('(' + str(i + 1) + '/' + str(sets) + ') ' + perk_desc[:s_index])
-                    i += 1
-                    perk_desc = perk_desc[s_index + 1:]
+            # if description is below twitch's character limit, send it to twitch chat
+            if len(perk_full) <= 500:
+                await ctx.send(perk_full)
+
+            # If greater than twitch's char limit, split it up
+            else:
+                # Check how many messages will need to be sent
+                sets = round(len(perk_desc)/500 + 0.5)
+
+                # Split up the description by the last space in each 500 characters
+                for i in range(sets):
+                    if i == 0:
+                        s_index = perk_desc[:491 - len(perk_name)].rfind(' ')
+                        await ctx.send(perk_name + ' (' + str(i + 1) + '/' + str(sets) + ') - ' + perk_desc[:s_index])
+                        i += 1
+                        perk_desc = perk_desc[s_index + 1:]
+                    elif i == sets - 1:
+                        await ctx.send('(' + str(i + 1) + '/' + str(sets) + ') - ' + perk_desc)
+                    else:
+                        s_index = perk_desc[:494].rfind(' ')
+                        await ctx.send('(' + str(i + 1) + '/' + str(sets) + ') - ' + perk_desc[:s_index])
+                        i += 1
+                        perk_desc = perk_desc[s_index + 1:]
+        
+        except AttributeError:
+            await ctx.send('No perk found!')
 
 bot = Bot()
 bot.run()
