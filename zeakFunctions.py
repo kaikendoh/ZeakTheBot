e372556df3f1a6213ace4ead790ec6cb296f61a4
Kai <107215518+kaikendoh@users.noreply.github.com>
2022-08-23 02:00:13 -0500

    separated out functions to separate python files

---
+++ zeakFunctions.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import gspread
+import pandas as pd
+from config import *
+
+# Import perks google sheets as dataframe
+sa = gspread.service_account(filename='service_account.json')
+sh = sa.open(GOOGLE_SHEET)
+
+def raid_scrape(msg):
+    wks = sh.worksheet('Raid Msg')
+    zeak_df = pd.DataFrame(wks.get_all_records())
+    
+    msg = msg.lower()
+    subMsg = zeak_df.loc[zeak_df['Command'] == msg, 'Subs'].values[0]
+    freeMsg = zeak_df.loc[zeak_df['Command'] == msg, 'Free'].values[0]
+
+    return subMsg, freeMsg
